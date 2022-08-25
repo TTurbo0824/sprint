d2ecf517b5d5b1de12fa0bb1f19bba87c3bef600
TTurbo0824 <429why@gmail.com>
2022-08-25 13:43:35 +0900

    add: Proxy setting

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,12 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    process.env.REACT_APP_API_DEFAULT,
-    createProxyMiddleware({
+    createProxyMiddleware('/api', {
       target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
       pathRewrite: {
-        [`^${process.env.REACT_APP_API_DEFAULT}`]: '',
+        '^/api': '',
       },
     }),
   );
